aba6b5edd1014da735b0b806ffbb1b52abfd3886
Andrei Ille <aille@sdl.com>
2018-09-13 14:40:35 +0300

    15 - dispatching actions with the fetched data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 //micro module for generatings unique id's
 import { v4 } from "node-uuid";
 
+export const receiveTodos = (filter, response) => ({
+  type: "RECEIVE_TODOS",
+  filter,
+  response
+});
+
 export const addTodo = text => ({
   type: "ADD_TODO",
   id: v4(),
--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { toggleTodo } from "../actions";
+import * as actions from "../actions";
 import { withRouter } from "react-router-dom";
 import TodoList from "../components/TodoList";
 import { getVisibleTodos } from "../reducers";
@@ -12,21 +12,23 @@ import { fetchTodos } from "../api";
 class VisibleTodoList extends Component {
   //this method runs only once - when the component is first rendered
   componentDidMount() {
-    fetchTodos(this.props.filter).then(todos =>
-      console.log(this.props.filter, todos)
-    );
+    this.fetchData();
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.filter !== prevProps.filter) {
-      fetchTodos(this.props.filter).then(todos =>
-        console.log(this.props.filter, todos)
-      );
+      this.fetchData();
     }
   }
 
+  fetchData() {
+    const { filter, receiveTodos } = this.props;
+    fetchTodos(filter).then(todos => receiveTodos(filter, todos));
+  }
+
   render() {
-    return <TodoList {...this.props} />;
+    const { toggleTodo, ...rest } = this.props;
+    return <TodoList onTodoClick={toggleTodo} {...rest} />;
   }
 }
 
@@ -39,20 +41,11 @@ const mapStateToProps = (state, { match }) => {
   };
 };
 
-// const mapDispatchToProps = dispatch => ({
-//   onTodoClick(id) {
-//     dispatch(toggleTodo(id));
-//   }
-// });
-
 // 'withRouter' is used to get access to the router/url params
 // withRouter and connect inject additional props to the presentational component
 export default (VisibleTodoList = withRouter(
   connect(
     mapStateToProps,
-    // mapDispatchToProps
-    // if the arguments passed to the callback props match the arguments passed to the action
-    // creator, then we can use this shorthand version of mapping dispatch to props
-    { onTodoClick: toggleTodo }
+    actions
   )(VisibleTodoList)
 ));
