18f3c5c7d23c7c3476505d5feb89c7211ab95c5b
Andrei Ille <aille@sdl.com>
2018-09-14 17:23:57 +0300

    17 - the middleware chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,46 +1,56 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-const addLoggingToDispatch = store => {
-  const rawDispatch = store.dispatch;
+const logger = store => next => {
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
 
   return action => {
     console.group(action.type);
     console.log("%c prev state", "color: gray", store.getState());
     console.log("%c action", "color: blue", action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log("%c next state", "color: green", store.getState());
     console.groupEnd(action.type);
     return returnValue;
   };
 };
 
-const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
+// this is just a function with several arguments that applied as they become available
+const promise = store => next => action => {
+  console.log("action:", action);
 
-  return action => {
-    console.log("action:", action);
+  if (typeof action.then === "function") {
+    //wait for the promise to resolve to an action object that we pass to next
+    return action.then(next);
+  }
+  return next(action);
+};
 
-    if (typeof action.then === "function") {
-      //wait for the promise to resolve to an action object that we pass to rawDispatch
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  //fix the order in which the middlewares are specified
+  //it would be more natural to speficy the order in which the action propagates through the middleware
+  middlewares
+    .slice()
+    .reverse()
+    .forEach(middleware => {
+      store.dispatch = middleware(store)(store.dispatch);
+    });
 };
 
 const configureStore = () => {
   const store = createStore(rootReducer);
+  //store.dispatch got replaced with an array of functions called 'middlewares'
+  const middlewares = [promise];
 
-  // ommit displaying logs in production
   if (process.env.NODE_ENV !== "production") {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+  //the purpose of the middlewares is to replace the single dispatch function with
+  //a chain of disposable dispatch functions which each do something with an action
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
